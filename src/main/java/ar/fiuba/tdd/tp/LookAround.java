b862e2536a6a85f18981be6720c99d58cc73c2a5
gabriel <ga-yo-so@hotmail.com>
2016-04-22 01:22:01 -0300

    clase para representar la accion 'mirar'

---
+++ src/main/java/ar/fiuba/tdd/tp/LookAround.java
@@ -0,0 +1,19 @@
+package ar.fiuba.tdd.tp;
+/**
+ * Created by Master on 21/04/2016.
+ */
+public class LookAround implements Action {
+
+    @Override
+    public String doAction(CanDoActions doer, CanHaveActionsDoneOn doee, Object[] args) {
+        if(!(doee instanceof CanBeLookedAt)){
+            return "invalid argument";
+        }
+        return ((CanBeLookedAt) doee).lookAt();
+    }
+
+    @Override
+    public String getName() {
+        return "look";
+    }
+}
