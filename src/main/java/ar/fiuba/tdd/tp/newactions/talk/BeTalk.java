ce6ee125c3b19c57294f006126b40299c144c5d8
fernandonitz <nitzfernando@gmail.com>
2016-04-27 20:31:26 -0300

    Se agrega el talk, guarda!!, gabo, chequea como lo arme, y hay q chequear los Sizes y el casteo!!!

---
+++ src/main/java/ar/fiuba/tdd/tp/newactions/talk/BeTalk.java
@@ -0,0 +1,42 @@
+package ar.fiuba.tdd.tp.newactions.talk;
+
+import ar.fiuba.tdd.tp.newactions.ActionHandler;
+import ar.fiuba.tdd.tp.objects.concrete.player.Player;
+import ar.fiuba.tdd.tp.objects.general.GameObject;
+
+import java.util.List;
+
+/**
+ * Created by fernando on 27/04/16.
+ */
+public class BeTalk extends ActionHandler {
+
+    private static int OBJECT_WHO_TALKS = 0;
+    private static int ARGS_SIZE = 1;
+
+    public BeTalk(GameObject instance) {
+        super(instance);
+    }
+
+    @Override
+    public String handleAction(String actionName, List<GameObject> objectsInvolved) {
+        if (objectsInvolved.get(0) instanceof Player){
+            Player oPlayer = (Player) objectsInvolved.get(0);
+            oPlayer.getChild("damn object");
+        }
+        return "done";
+    }
+
+    @Override
+    protected boolean canIHandleAction(List<GameObject> objectsInvolved) {
+        if (objectsInvolved.size() != ARGS_SIZE) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String getName() {
+        return "be talk";
+    }
+}
