391f75bb23ebf259bc0185ce38e896619f2a627c
Nicolás Keklikian <nkeklikian@gmail.com>
2016-04-28 02:20:06 -0300

    Corrijo bug de una linea

--- src/main/java/ar/fiuba/tdd/tp/actions/HaveMovedTo.java
+++ src/main/java/ar/fiuba/tdd/tp/actions/HaveMovedTo.java
@@ -14,8 +14,6 @@ public class HaveMovedTo extends ActionHandler {
     protected static int OBJECT_TO_MOVE = 1;
     protected static int ARGS_SIZE = 2;
 
-    protected ChildrenState children;
-
     public HaveMovedTo(GameObject instance, ChildrenState children) {
         super(instance);
         this.children = children;
