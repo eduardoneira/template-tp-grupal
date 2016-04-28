678b50a7917ddcf33c7370811903e32fe7e9e623
fernandonitz <nitzfernando@gmail.com>
2016-04-28 01:22:55 -0300

    OpenDoor2 y refactor para OpenDoor

--- src/main/java/ar/fiuba/tdd/tp/OpenDoor2.java
+++ src/main/java/ar/fiuba/tdd/tp/OpenDoor2.java
@@ -1,25 +1,28 @@
 package ar.fiuba.tdd.tp;
+
 import ar.fiuba.tdd.tp.actions.open.Open;
-import ar.fiuba.tdd.tp.objects.concrete.door.LockedDoor;
-
 import ar.fiuba.tdd.tp.actions.pick.Pick;
+import ar.fiuba.tdd.tp.objects.concrete.Box;
 import ar.fiuba.tdd.tp.objects.concrete.Room;
-
-import java.util.concurrent.locks.Lock;
+import ar.fiuba.tdd.tp.objects.concrete.door.LockedDoor;
 
 /**
  * Created by fernando on 28/04/16.
  */
-public class OpenDoor extends Game {
+public class OpenDoor2 extends Game {
 
     Room cuarto;
+    Box box;
     LockedDoor door;
     String nameRoom = "room";
     String nameKey = "key";
     String nameDoor = "door";
+    String nameBox = "box";
 
-    public OpenDoor() {
-        super("Open Door");
+    public OpenDoor2() {
+
+        super("Open Door 2");
+        box = new Box(nameBox);
         cuarto = new Room(nameRoom);
         door = new LockedDoor(nameDoor,1);
         keywords.add(nameKey);
@@ -31,8 +34,9 @@ public class OpenDoor extends Game {
         keywords.add(actionPickup.getName());
         player.addAction(actionPickup);
         player.addAction(actionOpen);
+
     }
 
     @Override
-    public boolean checkWinCondition() { return door.isOpen(); }
+    public boolean checkWinCondition() {return door.isOpen();}
 }
