6cdbbc1b817fa5c1d17eeddc147fc765c9fdd38b
fernandonitz <nitzfernando@gmail.com>
2016-04-28 00:00:50 -0300

    Motor devuelve al juego

--- src/main/java/ar/fiuba/tdd/tp/Motor.java
+++ src/main/java/ar/fiuba/tdd/tp/Motor.java
@@ -1,14 +1,29 @@
 package ar.fiuba.tdd.tp;
+import java.util.*;
 
 public class Motor {
 
-    public static String[] GAMESAVAILABLE = {"Fetch Quest",
-            "Open Door",
-            "Open Door 2",
-            "Cursed Object",
-            "Wolf, Sheep and Cabbage",
-            "Hanoi Towers",
-            "Treasure Hunt"};
+    private LinkedList<Game> games;
+
+    public Motor(String game) {
+
+        games=new LinkedList<Game> ();
+        games.add( new FetchQuest() );
+        //games.add( new OpenDoor() );
+        //games.add( new OpenDoor2() );
+        //games.add( new CursedObject() );
+        games.add( new WolfSheepCabbage() );
+        //games.add( new HanoiTowers() );
+        //games.add( new TreasureHunt() );
+    }
+    Game getGame(String game){
+        for (Game actualGame: games){
+            if ( game.equals(actualGame.getName()) ){
+                return actualGame;
+            }
+        }
+        return null;
+    }
 
     // TODO: Refactor para evitar switch, cuando este la factory posta hay que meterlo ahi y sale
     public static Game createGame(String game) {
@@ -19,9 +34,9 @@ public class Motor {
         }
     }
 
-    public static boolean isValidGame(String possibleGame) {
-        for ( String game : GAMESAVAILABLE) {
-            if ( possibleGame.toLowerCase().equals(game.toLowerCase())) {
+    private boolean isValidGame(String possibleGame) {
+        for ( Game actualGame: games) {
+            if ( possibleGame.toLowerCase().equals(actualGame.getName().toLowerCase())) {
                 return true;
             }
         }
