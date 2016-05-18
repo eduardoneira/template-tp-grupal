534275633cf58a849ced9a7f313b945ae4d72272
Nico Gago.- <nicogago.-@hotmail.com>
2016-05-18 00:29:18 -0300

    comienzo de mergeo it2

--- src/main/java/ar/fiuba/tdd/tp/Main.java
+++ src/main/java/ar/fiuba/tdd/tp/Main.java
@@ -1,7 +1,11 @@
 package ar.fiuba.tdd.tp;
 
+import ar.fiuba.tdd.tp.model.GameBuilder;
+import ar.fiuba.tdd.tp.server.BuilderLoader;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("This is just a tp project");
+    public static void main(String[] args) throws Exception {
+        GameBuilder builder = BuilderLoader.load(args[0]);
+        builder.build();
     }
 }
