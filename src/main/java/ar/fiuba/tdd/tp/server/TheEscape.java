2b677873946cc67ffc7573b916b8c3dcac7caa3c
Eduardo Neira <eduardo.neira@softtek.com>
2016-05-19 20:02:14 -0300

    puerta y habitaciones

--- src/main/java/ar/fiuba/tdd/tp/server/TheEscape.java
+++ src/main/java/ar/fiuba/tdd/tp/server/TheEscape.java
@@ -1,12 +1,49 @@
 package ar.fiuba.tdd.tp.server;
 
 import ar.fiuba.tdd.tp.model.Game;
+import ar.fiuba.tdd.tp.objects.concrete.Room;
+import ar.fiuba.tdd.tp.objects.concrete.door.LinkingDoor;
+import sun.awt.image.ImageWatched;
 
 /**
  * Created by eduardo.neira on 19/05/2016.
  */
 public class TheEscape extends Game {
-    
+
+    private Room pasillo;
+    private LinkingDoor pasilloToSalon1;
+    private LinkingDoor pasilloToSalon2;
+    private LinkingDoor pasilloToSalon3;
+    private LinkingDoor pasilloToBibliotecaAcceso;
+
+
+    private Room salon1;
+    private LinkingDoor salon1ToPasillo;
+
+    private Room salon2;
+    private LinkingDoor salon2ToPasillo;
+
+    private Room salon3;
+    private LinkingDoor salon3ToPasillo;
+
+    private Room bibliotecaAcceso;
+    private LinkingDoor bibliotecaAccesoToPasillo;
+    private LinkingDoor bibliotecaAccesoToBiblioteca;
+
+    private Room biblioteca;
+    private LinkingDoor bibliotecaToBibliotecaAcceso;
+    private LinkingDoor bibliotecaToSotano;
+
+    private Room sotano;
+    private LinkingDoor sotanoToBiblioteca;
+    private LinkingDoor sotanoToSotanoAbajo;
+
+    private Room sotanoAbajo;
+    private LinkingDoor sotanoAbajoToAfuera;
+
+    private Room afuera;
+
+
     public TheEscape(String name) {
         super(name);
     }
@@ -23,6 +60,7 @@ public class TheEscape extends Game {
 
     @Override
     public Game build() {
-        return null;
+
     }
+
 }
