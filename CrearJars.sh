df6ddc092f17e9dc0ccb51cbbeaab5766270cad9
Nico Gago.- <nicogago.-@hotmail.com>
2016-05-20 21:26:57 -0300

    creamos de nuevo bien los jars

--- CrearJars.sh
+++ CrearJars.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-jar cvf build/classes/main/ar/fiuba/tdd/tp/FetchQuest.jar build/classes/main/ar/fiuba/tdd/tp/FetchQuest.class
-jar cvf build/classes/main/ar/fiuba/tdd/tp/CursedObject.jar build/classes/main/ar/fiuba/tdd/tp/CursedObject.class
-jar cvf build/classes/main/ar/fiuba/tdd/tp/OpenDoor.jar build/classes/main/ar/fiuba/tdd/tp/OpenDoor.class
-jar cvf build/classes/main/ar/fiuba/tdd/tp/OpenDoor2.jar build/classes/main/ar/fiuba/tdd/tp/OpenDoor2.class
-jar cvf build/classes/main/ar/fiuba/tdd/tp/TorresHanoi.jar build/classes/main/ar/fiuba/tdd/tp/TorresHanoi.class
-jar cvf build/classes/main/ar/fiuba/tdd/tp/TreasureHunt.jar build/classes/main/ar/fiuba/tdd/tp/TreasureHunt.class
-jar cvf build/classes/main/ar/fiuba/tdd/tp/WolfSheepCabbage.jar build/classes/main/ar/fiuba/tdd/tp/WolfSheepCabbage.class
-jar cvf build/classes/main/ar/fiuba/tdd/tp/TheEscape.jar build/classes/main/ar/fiuba/tdd/tp/TheEscape.class
+cd build/classes/main
+
+jar cvf ar/fiuba/tdd/tp/FetchQuest.jar ar/fiuba/tdd/tp/FetchQuest.class
+jar cvf ar/fiuba/tdd/tp/CursedObject.jar ar/fiuba/tdd/tp/CursedObject.class
+jar cvf ar/fiuba/tdd/tp/OpenDoor.jar ar/fiuba/tdd/tp/OpenDoor.class
+jar cvf ar/fiuba/tdd/tp/OpenDoor2.jar ar/fiuba/tdd/tp/OpenDoor2.class
+jar cvf ar/fiuba/tdd/tp/TorresHanoi.jar ar/fiuba/tdd/tp/TorresHanoi.class
+jar cvf ar/fiuba/tdd/tp/TreasureHunt.jar ar/fiuba/tdd/tp/TreasureHunt.class
+jar cvf ar/fiuba/tdd/tp/WolfSheepCabbage.jar ar/fiuba/tdd/tp/WolfSheepCabbage.class
+jar cvf ar/fiuba/tdd/tp/TheEscape.jar ar/fiuba/tdd/tp/TheEscape.class
